a6ec70d4a31255b1b80d8a3bfe4bb5d635ee3cba
Faidy Sanmiguel <fjsanmiguel@icesi.edu.co>
2014-07-17 14:47:24 -0500

    Finalización de funcionalidad código de barras en el reporte inscritos

--- admin/src/ExportInscritos.php
+++ admin/src/ExportInscritos.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Este archivo se encarga de generar el reporte de asistencia a un evento
- * en PDF o EXCEL seg�n el parametr� enviado en la varible $_GET['rep_format'].
+ * en PDF o EXCEL según el parametró enviado en la varible $_GET['rep_format'].
  *
- * @author David Andr�s Manzano - damanzano
+ * @author David Andrés Manzano - damanzano
  * @since 15/02/11
  * @package src
  */
@@ -30,12 +30,12 @@ $hoy = strftime('%A, %d de %B de %Y')." - ".date('H:i');
 switch ($formato) {
     case 'pdf':
         /**
-         * Librearia para generai�n de PDF
+         * Librearia para generaión de PDF
          */
         include_once ('../lib/generadorpdf/class.ezpdf.php');
         // Creando el objeto PDF
         $pdf = new Cezpdf("letter", "landscape");
-        //L�mites X: 792, Y: 612
+        //Límites X: 792, Y: 612
         // Estableciendo las margenes
         $pdf->ezSetMargins(30, 30, 30, 30);
 
@@ -45,10 +45,10 @@ switch ($formato) {
         if (file_exists($imagen)) {
             $pdf->addJpegFromFile($imagen, 30, 552, 100, 32);
         }
-        //T�tulo
+        //Título
         $pdf->ezSetY(582);
         $pdf->selectFont("../lib/generadorpdf/fonts/Helvetica-Bold.afm");
-        $pdf->ezText("<b>SISTEMA DE GESTI�N DE EVENTOS</b>", 16, array('left' => 265));
+        $pdf->ezText("<b>SISTEMA DE GESTIÓN DE EVENTOS</b>", 16, array('left' => 265));
         $pdf->ezText("Reporte de inscritos al evento", 12, array('left' => 310));
         $pdf->selectFont("generadorpdf/fonts/Helvetica.afm");
         $pdf->ezText(" ", 10, array('left' => 0));
@@ -60,34 +60,34 @@ switch ($formato) {
         //$pdf->ezText(" RRAPWSALAS  - " . strtoupper($usuario[0]), 10);
         $pdf->ezSetY(535);
         $pdf->ezText($hoy, 10, array('justification' => 'centre'));
-        $pdf->ezStartPageNumbers(680, 35, 10, "right", "P�gina {PAGENUM} de {TOTALPAGENUM}");
-        //$pdf->ezText("P�gina ".$pdf->ezGetCurrentPageNumber()." de ", 10, array('justification'=>'right'));
+        $pdf->ezStartPageNumbers(680, 35, 10, "right", "Página {PAGENUM} de {TOTALPAGENUM}");
+        //$pdf->ezText("Página ".$pdf->ezGetCurrentPageNumber()." de ", 10, array('justification'=>'right'));
         $pdf->selectFont("../lib/generadorpdf/fonts/Helvetica.afm");
 
         $pdf->ezSetY(520);
         $pdf->ezText("Asistentes al evento : " . $titulo_evento, 10, array('left' => 0));
 
-        // Cerrando objetos que ir�n en todas las p�ginas
+        // Cerrando objetos que irán en todas las páginas
         $pdf->closeObject();
         $pdf->addObject($encabezado, 'all');
 
 
-        // Agregando objetos a todas las p�ginas
+        // Agregando objetos a todas las páginas
         $pdf->ezSetMargins(102, 50, 30, 30);
         $pdf->ezSetY(521);
 
         // Seleccionando fuente helvetica normal
         $pdf->selectFont("generadorpdf/fonts/Helvetica.afm");
 
-        // Asignando salto de l�nea a 10pt
+        // Asignando salto de línea a 10pt
         $pdf->ezSetDy(10);
         // Datos de la tabla
         $datos = $_SESSION['asistentes'];
-        // T�tulos para la tabla
-        $titulos = array('codigo_barras' => '<b>Código de barras</b>','username' => '<b>Nombre de usuario</b>', 'nombre' => '<b>Nombre</b>', 'apellido' => '<b>Apellido</b>', 'correo' => '<b>Correo</b>');
-        // Configuraci�n para la tabla
+        // Títulos para la tabla
+        $titulos = array('username' => '<b>Nombre de Usuario</b>', 'nombre' => '<b>Nombre</b>', 'apellido' => '<b>Apeliido</b>', 'correo' => '<b>Correo</b>');
+        // Configuración para la tabla
         $configuracion = array('showHeadings' => 1, 'shaded' => 0, 'showLines' => 2, 'width' => 730, 'xPos' => 35, 'xOrientation' => 'right');
-        // Escribiendo dos saltos de l�nea
+        // Escribiendo dos saltos de línea
         $pdf->ezText(" ");
         $pdf->ezText(" ");
         // Dibujando la tabla
@@ -123,7 +123,7 @@ switch ($formato) {
         // Creando la Hoja de trabajo
         $hoja_trabajo = &$libro_trabajo->addWorksheet($nombre_hoja);
 
-        // Creando el formato para los t�tulos de las columnas
+        // Creando el formato para los títulos de las columnas
         $formato_titulo = &$libro_trabajo->addFormat();
         // Negrilla
         $formato_titulo->setBold();
@@ -134,51 +134,53 @@ switch ($formato) {
         // Color fondo rojo
         $formato_titulo->setFgColor("red");
 
-        // Agregando los t�tulos de las columnas a la hoja de trabajo
+      // Agregando los t tulos de las columnas a la hoja de trabajo
         $hoja_trabajo->write(0, 0, 'ID', $formato_titulo);
-        $hoja_trabajo->write(0, 1, 'Nombre de usuario', $formato_titulo);
-        $hoja_trabajo->write(0, 2, 'Nombre', $formato_titulo);
-        $hoja_trabajo->write(0, 3, 'Apellidos', $formato_titulo);
-        $hoja_trabajo->write(0, 4, 'Correo', $formato_titulo);
-        $hoja_trabajo->write(0, 5, 'Lugar', $formato_titulo);
-        $hoja_trabajo->write(0, 6, 'Tel�fono', $formato_titulo);
-        $hoja_trabajo->write(0, 7, 'G�nero', $formato_titulo);
-        $hoja_trabajo->write(0, 8, 'Organizaci�n/Universidad', $formato_titulo);
-        $hoja_trabajo->write(0, 9, 'Tipo de inscripci�n', $formato_titulo);
-        $hoja_trabajo->write(0, 10, 'Fecha de inscripci�n', $formato_titulo);
-        $hoja_trabajo->write(0, 11, 'Fecha de pago', $formato_titulo);
-        $hoja_trabajo->write(0, 12, 'Solicitudes especiales', $formato_titulo);
+        $hoja_trabajo->write(0, 1, 'Codigo de barras', $formato_titulo);        
+        $hoja_trabajo->write(0, 2, 'Nombre de usuario', $formato_titulo);
+        $hoja_trabajo->write(0, 3, 'Nombre', $formato_titulo);
+        $hoja_trabajo->write(0, 4, 'Apellidos', $formato_titulo);
+        $hoja_trabajo->write(0, 5, 'Correo', $formato_titulo);
+        $hoja_trabajo->write(0, 6, 'Lugar', $formato_titulo);
+        $hoja_trabajo->write(0, 7, 'Tel fono', $formato_titulo);
+        $hoja_trabajo->write(0, 8, 'G nero', $formato_titulo);
+        $hoja_trabajo->write(0, 9, 'Organizaci n/Universidad', $formato_titulo);
+        $hoja_trabajo->write(0, 10, 'Tipo de inscripci n', $formato_titulo);
+        $hoja_trabajo->write(0, 11, 'Fecha de inscripci n', $formato_titulo);
+        $hoja_trabajo->write(0, 12, 'Fecha de pago', $formato_titulo);
+        $hoja_trabajo->write(0, 13, 'Solicitudes especiales', $formato_titulo);
         if(!empty($titulos_cp)){
-          $hoja_trabajo->write(0, 13, $titulos_cp[0], $formato_titulo);
-          $hoja_trabajo->write(0, 14, $titulos_cp[1], $formato_titulo);
-          $hoja_trabajo->write(0, 15, $titulos_cp[2], $formato_titulo);
-          $hoja_trabajo->write(0, 16, $titulos_cp[3], $formato_titulo);
-          $hoja_trabajo->write(0, 17, $titulos_cp[4], $formato_titulo);
+          $hoja_trabajo->write(0, 14, $titulos_cp[0], $formato_titulo);
+          $hoja_trabajo->write(0, 15, $titulos_cp[1], $formato_titulo);
+          $hoja_trabajo->write(0, 16, $titulos_cp[2], $formato_titulo);
+          $hoja_trabajo->write(0, 17, $titulos_cp[3], $formato_titulo);
+          $hoja_trabajo->write(0, 18, $titulos_cp[4], $formato_titulo);
         }
         // Agregando los datos a la hoja de trabajo
         $datos = $_SESSION['asistentes'];
         $fila = 1;
         foreach ($datos as $asistente) {
             $hoja_trabajo->writeString($fila, 0, $asistente['registration_id']);
-            $hoja_trabajo->writeString($fila, 1, $asistente['username']);
-            $hoja_trabajo->writeString($fila, 2, $asistente['nombre']);
-            $hoja_trabajo->writeString($fila, 3, $asistente['apellido']);
-            $hoja_trabajo->writeString($fila, 4, $asistente['correo']);
-            $hoja_trabajo->writeString($fila, 5, $asistente['mailing_address']);
-            $hoja_trabajo->writeString($fila, 6, $asistente['phone']);
-            $hoja_trabajo->writeString($fila, 7, $asistente['gender']);
-            $hoja_trabajo->writeString($fila, 8, $asistente['affiliation']);
-            $hoja_trabajo->writeString($fila, 9, $asistente['tipo_inscripcion']);
-            $hoja_trabajo->writeString($fila, 10, $asistente['date_registered']);
-            $hoja_trabajo->writeString($fila, 11, $asistente['date_paid']);
-            $hoja_trabajo->writeString($fila, 12, $asistente['special_requests']);
+            $hoja_trabajo->writeString($fila, 1, $asistente['codigo_barras']);
+            $hoja_trabajo->writeString($fila, 2, $asistente['username']);
+            $hoja_trabajo->writeString($fila, 3, $asistente['nombre']);
+            $hoja_trabajo->writeString($fila, 4, $asistente['apellido']);
+            $hoja_trabajo->writeString($fila, 5, $asistente['correo']);
+            $hoja_trabajo->writeString($fila, 6, $asistente['mailing_address']);
+            $hoja_trabajo->writeString($fila, 7, $asistente['phone']);
+            $hoja_trabajo->writeString($fila, 8, $asistente['gender']);
+            $hoja_trabajo->writeString($fila, 9, $asistente['affiliation']);
+            $hoja_trabajo->writeString($fila, 10, $asistente['tipo_inscripcion']);
+            $hoja_trabajo->writeString($fila, 11, $asistente['date_registered']);
+            $hoja_trabajo->writeString($fila, 12, $asistente['date_paid']);
+            $hoja_trabajo->writeString($fila, 13, $asistente['special_requests']);
             if(!empty($asistente['campos_personalizados'])){
               $campos = explode(";",$asistente['campos_personalizados']);
-              $hoja_trabajo->writeString($fila, 13, $campos[0]);
-              $hoja_trabajo->writeString($fila, 14, $campos[1]);
-              $hoja_trabajo->writeString($fila, 15, $campos[2]);
-              $hoja_trabajo->writeString($fila, 16, $campos[3]);
-              $hoja_trabajo->writeString($fila, 17, $campos[4]);
+              $hoja_trabajo->writeString($fila, 14, $campos[0]);
+              $hoja_trabajo->writeString($fila, 15, $campos[1]);
+              $hoja_trabajo->writeString($fila, 16, $campos[2]);
+              $hoja_trabajo->writeString($fila, 17, $campos[3]);
+              $hoja_trabajo->writeString($fila, 18, $campos[4]);
             }
             $fila++;
         }
