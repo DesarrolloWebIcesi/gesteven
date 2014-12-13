33c7b3fbbf3cd40da3780cd7ed112595fea51c28
David Andres Manzano Herrera <damh24@gmail.com>
2014-12-13 11:35:34 -0500

    - Fix Google Chrome "Mixed content" warning

--- admin/gui/cabezote.php
+++ admin/gui/cabezote.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * HTML requerido para el despliegue del cabezote de la aplicación.
+ * HTML requerido para el despliegue del cabezote de la aplicaciÃ³n.
  * @package gui
  */
 ?>
 <div id="encabezado">
-  <img src="http://www.icesi.edu.co/eventos/admin/imgs/cabe_ojs.jpg" alt="Servicios de Apoyo Acad&eacute;mico y Administrativo" usemap="#Map" title="Servicios de Apoyo Acad&eacute;mico y Administrativo" border="0" style="margin: 0 auto;"/>
+  <img src="//www.icesi.edu.co/eventos/admin/imgs/cabe_ojs.jpg" alt="Servicios de Apoyo Acad&eacute;mico y Administrativo" usemap="#Map" title="Servicios de Apoyo Acad&eacute;mico y Administrativo" border="0" style="margin: 0 auto;"/>
   <map name="Map" id="Map">
     <area shape="rect" coords="32,25,191,77" href="http://www.icesi.edu.co/" target="_blank" alt="Universidad Icesi" />
     <area shape="rect" coords="462,31,960,50" href="http://www.icesi.edu.co/servicios_apoyo/" target="_self" alt="Servicios de Apoyo Acad&eacute;mico y Administrativo" />
